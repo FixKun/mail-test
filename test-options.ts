a015214029535d160e41e9e9fbe9e99cfb716918
ruslanzaystev <ruslan.zaystev@planixs.com>
2024-12-24 11:02:19 +0000

    fixturise PageManager

---
+++ test-options.ts
@@ -0,0 +1,21 @@
+import {test as base} from '@playwright/test'
+import { PageManager } from "./page-objects/pageManager"
+
+export type TestOptions = {
+    navigateToMainPage: string,
+    pageManager: PageManager
+}
+
+export const test = base.extend<TestOptions>({
+
+    navigateToMainPage: async({page}, use) => {
+        await page.goto('/flatx/index.jsp')
+        await use('')
+    },
+
+    pageManager: async({page, navigateToMainPage}, use) => {
+        const pm = new PageManager(page)
+        await use(pm)
+    }
+
+})
--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,33 +1,29 @@
-import { test } from '@playwright/test'
-import { PageManager } from '../page-objects/pageManager'
+import { test } from '../test-options'
 import { createRandomTextFile } from './helpers'
 import data from '../data/mailTest.json'
 
-test('mail-001 @smoke', async ({page}) => {
-    const pm = new PageManager(page)
-
-    await page.goto('/flatx/index.jsp')
+test('mail-001 @smoke', async ({pageManager}) => {
 
     const dirPath = './data'
     const fileName = createRandomTextFile(dirPath)
     process.env.FILE_PATH = `${dirPath}/${fileName}`
 
-    const unreadCount = await pm.onMainMailPage().getUnreadCount()
+    const unreadCount = await pageManager.onMainMailPage().getUnreadCount()
 
-    await pm.onMainMailPage().clickCreateNewEmailButton()
-    await pm.onMailPage().setMailRecipient(data.destinationEmail)
-    await pm.onMailPage().setMailSubject(data.mailSubject)
-    await pm.onMailPage().attachFile(`${dirPath}/${fileName}`)
-    await pm.onMailPage().sendEmail()
+    await pageManager.onMainMailPage().clickCreateNewEmailButton()
+    await pageManager.onMailPage().setMailRecipient(data.destinationEmail)
+    await pageManager.onMailPage().setMailSubject(data.mailSubject)
+    await pageManager.onMailPage().attachFile(`${dirPath}/${fileName}`)
+    await pageManager.onMailPage().sendEmail()
 
-    await pm.onMainMailPage().waitForMailCountToIncrease(unreadCount, 300, 50)
-    await pm.onMainMailPage().selectUnreadEmailBySubject(data.mailSubject)
+    await pageManager.onMainMailPage().waitForMailCountToIncrease(unreadCount, 300, 50)
+    await pageManager.onMainMailPage().selectUnreadEmailBySubject(data.mailSubject)
     
-    await pm.onViewMailPage().saveAttachmentByNameInMyDocuments(fileName)
+    await pageManager.onViewMailPage().saveAttachmentByNameInMyDocuments(fileName)
 
-    await pm.onHeaderPage().goToDocs()
+    await pageManager.onHeaderPage().goToDocs()
 
-    await pm.onDocsPage().dragAndDropDocumentToFolderByName(fileName, 'Trash')
-    await pm.onDocsPage().openFolderByName('Trash')
-    await pm.onDocsPage().documentByNameExists(fileName)
+    await pageManager.onDocsPage().dragAndDropDocumentToFolderByName(fileName, 'Trash')
+    await pageManager.onDocsPage().openFolderByName('Trash')
+    await pageManager.onDocsPage().documentByNameExists(fileName)
 })
