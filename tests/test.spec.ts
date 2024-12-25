1c70698999a70a064e1b69bd1e3d94b1359659a4
ruslanzaystev <ruslan.zaystev@planixs.com>
2024-12-25 13:05:30 +0000

    use test steps

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -6,27 +6,47 @@ import data from '../data/mailTest.json'
 
 test('mail-001 @smoke', async ({pageManager}) => {
 
-    const dirPath = './data'
-    const fileName = createRandomTextFile(dirPath)
-    process.env.FILE_PATH = `${dirPath}/${fileName}`
-
-    const unreadCount = await pageManager.onMainMailPage().getUnreadCount()
-
-    await pageManager.onMainMailPage().clickCreateNewEmailButton()
-    await pageManager.onMailPage().setMailRecipient(data.destinationEmail)
-    await pageManager.onMailPage().setMailSubject(data.mailSubject)
-    await pageManager.onMailPage().attachFile(`${dirPath}/${fileName}`)
-    await pageManager.onMailPage().sendEmail()
-
-    await pageManager.onMainMailPage().waitForMailCountToIncrease(unreadCount)
-    await pageManager.onMainMailPage().selectUnreadEmailBySubject(data.mailSubject)
-    
-    await pageManager.onViewMailPage().saveAttachmentByNameInMyDocuments(fileName)
-
-    await pageManager.onHeaderPage().goToDocs()
-
     const trashFolderName = folderNames.trash
-    await pageManager.onDocsPage().dragAndDropDocumentToFolderByName(fileName, trashFolderName)
-    await pageManager.onDocsPage().openFolderByName(trashFolderName)
-    await pageManager.onDocsPage().documentByNameExists(fileName)
+    const dirPath = './data'
+
+    const fileName = await test.step(`Create random file`, async () => {
+        return createRandomTextFile(dirPath)
+    })
+    const filePath = `${dirPath}/${fileName}`
+    process.env.FILE_PATH = filePath
+
+    const unreadCount = await test.step(`Get unread emails count`, async () => {
+        return await pageManager.onMainMailPage().getUnreadCount()
+    })
+
+    await test.step(`Create and send email with subject: ${data.mailSubject} to ${data.destinationEmail}`, async () => {
+        await pageManager.onMainMailPage().clickCreateNewEmailButton()
+        await pageManager.onMailPage().setMailRecipient(data.destinationEmail)
+        await pageManager.onMailPage().setMailSubject(data.mailSubject)
+        await pageManager.onMailPage().attachFile(filePath)
+        await pageManager.onMailPage().sendEmail()
+    })
+
+    await test.step(`Wait for unread counter to increase`, async () => {
+        await pageManager.onMainMailPage().waitForMailCountToIncrease(unreadCount)
+    })
+
+    await test.step(`Get the latest unread email and save attachment to documents`, async () => {
+        await pageManager.onMainMailPage().selectUnreadEmailBySubject(data.mailSubject)
+        await pageManager.onViewMailPage().saveAttachmentByNameInMyDocuments(fileName)
+    })
+
+    await test.step(`Navigate to Documents page`, async () => {
+        await pageManager.onHeaderPage().goToDocs()
+    })
+
+    await test.step(`Drag'n'drop ${fileName} document to ${trashFolderName} folder`, async () => {
+        await pageManager.onDocsPage().dragAndDropDocumentToFolderByName(fileName, trashFolderName)
+    })
+
+    await test.step(`Validate that ${fileName} document is in ${trashFolderName} folder`, async () => {
+        await pageManager.onDocsPage().openFolderByName(trashFolderName)
+        await pageManager.onDocsPage().documentByNameExists(fileName)
+    })
+
 })
